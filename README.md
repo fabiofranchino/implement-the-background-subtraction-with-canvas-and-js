2c46ca55866463c4c9cf855f7f89f72e215fdd98
abusedmedia <ff@todo.to.it>
2019-10-25 13:18:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Webpack based webapp starterkit
+
+This is a webpack based empty project with a configuration that suits my common needs for simple web project or experiments.
+
+Here the list of my needs:
+
+- ES6 based
+- loader for CSS, SVG, CSV and images
+- Hot replacement during dev
+- static files handled properly
+
+
+## Init
+
+    npm install
+
+## Dev
+
+    npm start
+
+## Build
+
+    npm build
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  resolve: {
+    extensions: ['.js']
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hot',
+      filename: 'index.html',
+      template: 'template.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyPlugin([
+      { from: 'assets', to: 'assets' }
+    ])
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
